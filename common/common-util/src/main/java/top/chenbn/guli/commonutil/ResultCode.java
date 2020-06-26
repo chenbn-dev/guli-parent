9e29507382075e5bd44685faf360e2036308c54e
chenbainian <284049379@qq.com>
2020-06-26 12:08:01 +0800

    完成统一结果返回

---
+++ common/common-util/src/main/java/top/chenbn/guli/commonutil/ResultCode.java
@@ -0,0 +1,10 @@
+package top.chenbn.guli.commonutil;
+
+/**
+ * @author chbn
+ * @create 2020-06-26 10:55
+ */
+public interface ResultCode {
+  public static Integer SUCCESS = 20000;
+  public static Integer ERROR = 20001;
+}
--- service/service-edu/src/main/java/top/chenbn/guli/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/top/chenbn/guli/controller/EduTeacherController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import top.chenbn.guli.commonutil.Result;
 import top.chenbn.guli.entity.EduTeacher;
 import top.chenbn.guli.service.EduTeacherService;
 
@@ -24,21 +25,25 @@ public class EduTeacherController {
   // 1.查询讲师表中的所有数据
   @ApiOperation(value = "获取所有讲师列表")
   @GetMapping("/findAll")
-  public List<EduTeacher> findAllTeacher() {
+  public Result findAllTeacher() {
     // 调用service的方法实现查询所有的操作
-    List<EduTeacher> eduTeacherList = teacherService.list(null);
-    return eduTeacherList;
+    List<EduTeacher> list = teacherService.list(null);
+    return Result.ok().data("items", list);
   }
 
   // 2 逻辑删除讲师的方法
   @ApiOperation(value = "删除指定id的讲师")
   @DeleteMapping("/{id}") // {id} 表示id需要通过路径进行传递 localhost:8001/edu/1
-  public boolean removeTeacher(
+  public Result removeTeacher(
       @ApiParam(name = "id", value = "讲师ID", required = true)
           // @PathVariable 获取路径中输入的id值
           @PathVariable
           String id) {
     boolean flag = teacherService.removeById(id);
-    return flag;
+    if (flag) {
+      return Result.ok();
+    } else {
+      return Result.error();
+    }
   }
 }
