c56a4e6f9a3ea401afb19b49056ce40642546b18
chenbainian <284049379@qq.com>
2020-07-31 12:46:28 +0800

    数据同步工具

---
+++ canal-clientedu/src/main/java/top/chenbn/cancal/CanalApplication9001.java
@@ -0,0 +1,27 @@
+package top.chenbn.cancal;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import top.chenbn.cancal.client.CanalClient;
+
+import javax.annotation.Resource;
+
+/**
+ * @author chbn
+ * @create 2020-07-30 13:03
+ */
+@SpringBootApplication
+public class CanalApplication9001 implements CommandLineRunner {
+  @Resource private CanalClient canalClient;
+
+  @Override
+  public void run(String... strings) throws Exception {
+    // 项目启动，执行canal客户端监听
+    canalClient.run();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(CanalApplication9001.class, args);
+  }
+}
--- service/service-statistics/src/main/java/top/chenbn/guli/StaApplication8008.java
+++ service/service-statistics/src/main/java/top/chenbn/guli/StaApplication8008.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author chbn
  * @create 2020-07-28 12:59
  */
+@EnableScheduling
 @EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient
